ea81b7aaceba2cdb38576029e25929edf860cdf8
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-03-23 18:25:05 +0000

    try this

--- helpers.R
+++ helpers.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 stopgap <- readRDS("data/stopgap.rds")
 
 pvalue <- 1e-4
@@ -8,8 +10,8 @@ stopgap <- stopgap %>%
 	select(gene, msh, snp.ld, pvalue, gene.score, gene.rank.min, pubmedid) %>%
 	rename(Gene=gene, Trait=msh, SNP=snp.ld, Pvalue=pvalue, GeneScore=gene.score, GeneRank=gene.rank.min, PubMedID=pubmedid) %>%
 	filter(Pvalue < pvalue, GeneScore >= genescore, GeneRank <= generank) %>%
+	group_by(Gene, SNP, PubMedID) %>%
+	summarise(Pvalue=median(Pvalue), GeneScore=median(GeneScore), GeneRank=median(GeneRank)) %>%
 	unique
 
 saveRDS(stopgap, "data/smallgap.rds")
-
-
