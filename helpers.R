a92abfeb89f4b3b5bc026a05d0857eb7c089e009
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-04-28 17:36:21 +0100

    update after showing to Matt

--- helpers.R
+++ helpers.R
@@ -2,16 +2,17 @@ library(dplyr)
 
 stopgap <- readRDS("data/stopgap.rds")
 
-pvalue <- 1e-4
-genescore <- 3
-generank <- 5
+pvalue <- 1e-6
+genescore <- 6
+generank <- 1
 
-stopgap <- stopgap %>%
+smallgap <- stopgap %>%
 	select(gene, msh, snp.ld, pvalue, gene.score, gene.rank.min, pubmedid) %>%
 	rename(Gene=gene, Trait=msh, SNP=snp.ld, Pvalue=pvalue, GeneScore=gene.score, GeneRank=gene.rank.min, PubMedID=pubmedid) %>%
 	filter(Pvalue < pvalue, GeneScore >= genescore, GeneRank <= generank) %>%
-	group_by(Gene, SNP, PubMedID) %>%
+	group_by(Gene, SNP, Trait, PubMedID) %>%
 	summarise(Pvalue=median(Pvalue), GeneScore=median(GeneScore), GeneRank=median(GeneRank)) %>%
-	unique
+	select(Gene, SNP, Trait, Pvalue, GeneScore, GeneRank, PubMedID) %>%
+	distinct
 
-saveRDS(stopgap, "data/smallgap.rds")
+saveRDS(smallgap, "data/smallgap.rds")
