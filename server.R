b02fe89eff6b7a9ef45f65e38f10b199762b356d
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-05-28 12:37:08 +0100

    remove p-value slider

--- server.R
+++ server.R
@@ -8,24 +8,24 @@ shinyServer(function(input, output) {
   output$table <- renderDataTable({
 
     if (input$gene != "All") {
-        sqlCode <- paste0("select * from swapp where PValue >=", input$pvalue[1], " and PValue <=", input$pvalue[2],
+        sqlCode <- paste0("select * from swapp where PValue <=", input$pvalue,
                         " and GeneRank >=", input$generank[1], " and GeneRank <=", input$generank[2],
                         " and GeneScore >=", input$genescore[1], " and GeneRank <=", input$genescore[2],
                         " and Gene =", input$gene, ";")
     }
     if (input$trait != "All") {
-        sqlCode <- paste0("select * from swapp where PValue >=", input$pvalue[1], " and PValue <=", input$pvalue[2],
+        sqlCode <- paste0("select * from swapp where PValue <=", input$pvalue,
                         " and GeneRank >=", input$generank[1], " and GeneRank <=", input$generank[2],
                         " and GeneScore >=", input$genescore[1], " and GeneRank <=", input$genescore[2],
                         " and Trait =", input$trait, ";")
     }
     if (input$snp != "All") {
-        sqlCode <- paste0("select * from swapp where PValue >=", input$pvalue[1], " and PValue <=", input$pvalue[2],
+        sqlCode <- paste0("select * from swapp where PValue <=", input$pvalue,
                         " and GeneRank >=", input$generank[1], " and GeneRank <=", input$generank[2],
                         " and GeneScore >=", input$genescore[1], " and GeneRank <=", input$genescore[2],
                         " and SNP =", input$snp, ";")
     } else {
-        sqlCode <- paste0("select * from swapp where PValue >=", input$pvalue[1], " and PValue <=", input$pvalue[2],
+        sqlCode <- paste0("select * from swapp where PValue <=", input$pvalue,
                         " and GeneRank >=", input$generank[1], " and GeneRank <=", input$generank[2],
                         " and GeneScore >=", input$genescore[1], " and GeneRank <=", input$genescore[2], ";")
     }
