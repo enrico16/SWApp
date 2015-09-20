ea25b76f28498ef2d520554f64907c7d4d3549fc
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-09-20 12:57:16 +0100

    add link to PubMed

--- server.R
+++ server.R
@@ -3,6 +3,8 @@ library(DT)
 library(RMySQL)
 
 con <- dbConnect(MySQL(), user="ef884766", host="localhost", dbname="swapp")
+toDisplay <- c("Gene", "SNP", "Trait", "PValue", "GeneScore", "GeneRank", "PubMedLink")
+toDownload <- c("Gene", "SNP", "Trait", "PValue", "GeneScore", "GeneRank", "PubMedID")
 
 shinyServer(function(input, output) {
 				
@@ -14,15 +16,17 @@ shinyServer(function(input, output) {
 									  " and GeneRank >= ", input$generank[1], " and GeneRank <= ", input$generank[2],
 									  " and GeneScore >= ", input$genescore[1], " and GeneScore <= ", input$genescore[2],
 									  ";")
-					dbGetQuery(con, sqlCode)
+					sqlOutput <- dbGetQuery(con, sqlCode)
+                    sqlOutput$PubMedLink <- paste0("<a href='http://www.ncbi.nlm.nih.gov/pubmed/", sqlOutput$PubMedID, "'>", sqlOutput$PubMedID, "</a>")
+                    return(sqlOutput)
 
 				})
 				
-				output$table1 <- DT::renderDataTable(sqlOutput1(), server=TRUE, rownames=TRUE, filter="top", options=list(pageLength=10))
+				output$table1 <- DT::renderDataTable(sqlOutput1()[toDisplay], server=TRUE, rownames=TRUE, escape=FALSE, filter="top", options=list(pageLength=10))
 				
 				output$download1 <- downloadHandler("SWApp.csv", content = function(file) {
 										   rows <- input$table1_rows_all
-										   write.csv(sqlOutput1()[rows, ], file, quote=FALSE, col.names=TRUE, row.names=FALSE)
+										   write.csv(sqlOutput1()[rows, toDownload], file, quote=TRUE, col.names=TRUE, row.names=FALSE)
 				})
 
 				# STOPGAP Best LD
@@ -33,15 +37,17 @@ shinyServer(function(input, output) {
 									  " and GeneRank >= ", input$generank[1], " and GeneRank <= ", input$generank[2],
 									  " and GeneScore >= ", input$genescore[1], " and GeneScore <= ", input$genescore[2],
 									  ";")
-					dbGetQuery(con, sqlCode)
+					sqlOutput <- dbGetQuery(con, sqlCode)
+                    sqlOutput$PubMedLink <- paste0("<a href='http://www.ncbi.nlm.nih.gov/pubmed/", sqlOutput$PubMedID, "'>", sqlOutput$PubMedID, "</a>")
+                    return(sqlOutput)
 
 				})
 				
-				output$table2 <- DT::renderDataTable(sqlOutput2(), server=TRUE, rownames=TRUE, filter="top", options=list(pageLength=10))
+				output$table2 <- DT::renderDataTable(sqlOutput2()[toDisplay], server=TRUE, rownames=TRUE, escape=FALSE, filter="top", options=list(pageLength=10))
 				
 				output$download2 <- downloadHandler("SWApp.csv", content = function(file) {
 										   rows <- input$table2_rows_all
-										   write.csv(sqlOutput2()[rows, ], file, quote=FALSE, col.names=TRUE, row.names=FALSE)
+										   write.csv(sqlOutput2()[rows, toDownload], file, quote=TRUE, col.names=TRUE, row.names=FALSE)
 				})
 				
 
@@ -53,15 +59,17 @@ shinyServer(function(input, output) {
 									  " and GeneRank >= ", input$generank[1], " and GeneRank <= ", input$generank[2],
 									  " and GeneScore >= ", input$genescore[1], " and GeneScore <= ", input$genescore[2],
 									  ";")
-					dbGetQuery(con, sqlCode)
+					sqlOutput <- dbGetQuery(con, sqlCode)
+                    sqlOutput$PubMedLink <- paste0("<a href='http://www.ncbi.nlm.nih.gov/pubmed/", sqlOutput$PubMedID, "'>", sqlOutput$PubMedID, "</a>")
+                    return(sqlOutput)
 
 				})
 				
-				output$table3 <- DT::renderDataTable(sqlOutput3(), server=TRUE, rownames=TRUE, filter="top", options=list(pageLength=10))
+				output$table3 <- DT::renderDataTable(sqlOutput3()[toDisplay], server=TRUE, rownames=TRUE, escape=FALSE, filter="top", options=list(pageLength=10))
 				
 				output$download3 <- downloadHandler("SWApp.csv", content = function(file) {
 										   rows <- input$table3_rows_all
-										   write.csv(sqlOutput3()[rows, ], file, quote=FALSE, col.names=TRUE, row.names=FALSE)
+										   write.csv(sqlOutput3()[rows, toDownload], file, quote=TRUE, col.names=TRUE, row.names=FALSE)
 				})
 				
 
